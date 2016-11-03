f635d34fd13af9cdea39228935e455c04a9ceb46
jedrula <andrzej.swaton@gmail.com>
2016-11-03 20:34:32 +0100

    delete route

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const router = require('./router')(app, db);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
--- router.js
+++ router.js
@@ -10,11 +10,11 @@ module.exports = (app, db) => {
 }
 
 function _pathHandler(app, db, path) {
-  console.log('path handler for ', path)
   app.route(path)
       .post(_postHandler.bind(null, db, path))
       .get(_getHandler.bind(null, db, path));
-      // TODO add other handlers - delete, update, read
+  app.route(`${path}/:id`)
+      .delete(_deleteHandler.bind(null, db, path));
 }
 
 function _postHandler(db, path, req, res) {
@@ -44,6 +44,18 @@ function _getHandler(db, path, req, res) {
   });
 }
 
+function _deleteHandler(db, path, req, res) {
+  const { model, serializer } = db[path];
+  const id = req.params.id;
+  _deleteFromDb({ model, id }, (dbError, obj) => {
+    if (dbError) {
+      res.status(500).json(dbError);
+    } else {
+      res.sendStatus(204);
+    }
+  });
+}
+
 function _saveToDb(options, callback) {
   const { model, data } = options;
   const instance = new model(data);
@@ -53,4 +65,9 @@ function _saveToDb(options, callback) {
 function _getFromDb(options, callback) {
   const { model, path } = options;
   model.find({}, callback);
+}
+
+function _deleteFromDb(options, callback) {
+  const { model, id } = options;
+  model.remove({_id: id}, callback);
 }
