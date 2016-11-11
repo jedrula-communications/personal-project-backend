2c3bfdea4389ccd8e035e02d80d32aae1ae88b19
jedrula <andrzej.swaton@gmail.com>
2016-11-11 16:36:34 +0100

    simplify courtesy of json:api node module

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-// https://www.npmjs.com/package/kraken-js
-console.log('started');
+const dbuser = 'jedrek';
+const dbpassword = process.env.MONGO_PASS;
+const mongoDbUrl = `mongodb://${dbuser}:${dbpassword}@ds031628.mlab.com:31628/jedrek`;
+var jsonApi = require("jsonapi-server");
+var MongoStore = require("jsonapi-store-mongodb");
 
-const express = require('express');
-const bodyParser = require('body-parser')
-
-const app = express();
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
-
-const db = require('./db');
-const router = require('./router')(app, db);
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
-  next();
+jsonApi.setConfig({
+  port: process.env.PORT || 3000,
+  graphiql: true
+});
+jsonApi.define({
+  resource: "posts",
+  handlers: new MongoStore({
+    url: mongoDbUrl,
+  }),
+  attributes: {
+    // TODO add date
+    // TODO add author
+    title: jsonApi.Joi.string(),
+    category: jsonApi.Joi.string(),
+    body: jsonApi.Joi.string(),
+  }
 });
 
-router.init();
-
-// process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 3000;
-
-app.listen(port);
+jsonApi.start();
