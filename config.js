3ec2715faa312fc3bb2c985221608383190de351
jedrula <andrzej.swaton@gmail.com>
2017-02-24 21:39:56 +0100

    configurable mongodb

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const config = require('config');
+const mongoDbLocation = config.get('mongoDbLocation');//'192.168.0.155:27017/jedrek';
 module.exports = {
-  mongoDbUrl: 'mongodb://localhost:27017/jedrek',
+  mongoDbUrl: `mongodb://${mongoDbLocation}`,
 }
